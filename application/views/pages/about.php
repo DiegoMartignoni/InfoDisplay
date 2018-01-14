7f67c1e9947c40843686236e17d82b2062df8f96
Diego <m.s.d.d.diego@gmail.com>
2018-01-14 20:07:42 +0100

    creazione prima pagina statica (controller e routing)

---
+++ application/views/pages/about.php
@@ -0,0 +1,1 @@
+<h3>Questa pagina parla di me</h3>
---
+++ application/views/templates/header.php
@@ -0,0 +1,13 @@
+<html>
+<!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+  <head>
+    <title>CodeIgniter Tutorial</title>
+  </head>
+  <body>
+
+    <h1><?php echo $title; ?></h1>
