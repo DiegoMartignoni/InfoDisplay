b742cafb0279d0350a763c062a61ac191c0f576e
Amministratore <Administrator@testwin8.fbm.local>
2018-02-12 19:02:03 +0100

    creata il controller per gestire i singoli annunci con una view personale

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -9,4 +9,16 @@ class Posts extends CI_Controller {
     $this->load->view('posts/index', $data);
     $this->load->view('templates/footer', $data);
   }
+
+  public function view($slug = null)
+  {
+    $data['post'] = $this->Post_model->get_posts($slug);
+    if (empty($data['post'])) {
+      show_404();
+    }
+    $data['titolo'] = $data['post']['titolo'];
+    $this->load->view('templates/header');
+    $this->load->view('posts/view', $data);
+    $this->load->view('templates/footer');
+  }
 }
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,11 +3,13 @@
     <div class="row">
       <?php foreach ($posts as $post) : ?>
         <div class="col-md-4">
-          <div class="container jumbotron" style="width: 18rem;">
-            <small><?php echo $post['creattoAlle']?></small>
-            <h6><strong><?php echo $post['titolo']?></strong></h6>
-            <small><?php $taglio = substr($post['corpo'],0,100).'...'; echo $taglio?></small>
-          </div>
+          <a href="<?php echo site_url('posts/'.$post['slug']);?>" style="text-decoration: none" class="text-white">
+            <div class="container jumbotron" style="width: 18rem;">
+              <small><?php echo $post['creattoAlle']?></small>
+              <h6><strong><?php echo $post['titolo']?></strong></h6>
+              <small><?php $taglio = substr($post['corpo'],0,100).'...'; echo $taglio?></small>
+            </div>
+          </a>
         </div>
       <?php endforeach; ?>
     </div>
