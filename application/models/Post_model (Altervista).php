08f116f6da343e913a36bc46db93c18f8c9040e2
Amministratore <Administrator@testwin8.fbm.local>
2018-02-13 12:18:27 +0100

    creato file per altervista

--- application/models/Post_model (Altervista).php
+++ application/models/Post_model (Altervista).php
@@ -13,4 +13,16 @@
             $query = $this->db->get_where('CiAnnunci', array('slug' => $slug));
             return $query->row_array();
         }
+
+				public function create_post(){
+        	$slug = url_title($this->input->post('titolo'));
+					$data = array(
+						'titolo' => $this->input->post('titolo'),
+						'slug' => $slug,
+						'corpo' => $this->input->post('corpo'),
+
+					);
+
+					return $this->db->insert('CiAnnunci', $data);
+        }
     }
