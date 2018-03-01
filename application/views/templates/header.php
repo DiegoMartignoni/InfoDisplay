b2690aa8d4fe8309a27dbb5faa44d9c2e2a5464d
Diego <m.s.d.d.diego@gmail.com>
2018-03-01 17:34:05 +0100

    aggiornamento doc

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,7 +20,7 @@
   </head>
   <body class="w-100 h-100 text-center">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/ci">CodeIgniter</a>
+      <a class="navbar-brand" href="/ci">InfoDisplay</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- index.rst
+++ index.rst
@@ -9,5 +9,6 @@ Benvenuto nella documentazione di InfoDisplay!
 .. toctree::
    :maxdepth: 2
    :caption: Indice:
+   Specifiche tecniche
 
    Introduzione
